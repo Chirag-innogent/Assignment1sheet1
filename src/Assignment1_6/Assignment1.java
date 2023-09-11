8b60c01442495851f01958018bf8ce7f26341c7a
Chirag-innogent <chirag.tongia@innogent.in>
2023-09-11 14:06:21 +0530

    sheet1 Assignment1

--- src/Assignment1_6/Assignment1.java
+++ src/Assignment1_6/Assignment1.java
@@ -5,6 +5,7 @@ import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Assignment1 {
     public static void main(String[] args) {
@@ -21,19 +22,16 @@ public class Assignment1 {
         Services services= new Services();
         //Question 1
         List<Student> students1 = services.findByPincode(students,452002,addresses);
-        for(Student s:students1){
-            System.out.println(s);
-        }
+        students1.stream().forEach(System.out::println);
         System.out.println("-----------------");
 
         //Question2
         List<Student> studentList= services.findByCity(students,"Indore",addresses);
-        for(Student s:studentList){
-            System.out.println(s);
-        }
-        System.out.println("-----------------");
+        studentList.stream().forEach(System.out::println);
+        System.out.println("*************************");
         //Question3
-        services.reportCard(studentList);
+        services.reportCard(students);
+        System.out.println("----------Sorted-------");
         //Question4
         List<Student> studentPassed= services.listOfStudentPassed(students);
         System.out.println(studentPassed);
@@ -44,9 +42,35 @@ public class Assignment1 {
         List<Student>  studentInSeccion= services.findStudentByClass(students,classes,"A");
         System.out.println("section"+studentInSeccion);
         //Question8
+        System.out.println("Question 8");
         List<Student> studentFailByAge= services.failByAge(students);
         System.out.println(studentFailByAge);
         //Question 9
+    /*    int studentId=1;
+         Student student1 = studentList.stream().filter(e -> e.getId() == studentId).findFirst().get();
+        List<Address>addressList = addresses.stream().filter(address -> address.getStudentId()==studentId).collect(Collectors.toList());
+        students.remove(student1);
+        addresses.removeAll(addressList);
+*/
+        //Question 10
+        if(students.isEmpty())
+            students=null;
+        if(addresses.isEmpty())
+            addresses=null;
+
+        //Question11
+        System.out.println("Question 11");
+        List<Student> femaleStudentsPage1To9 = services.pagenation(students,1,9,"F");
+        femaleStudentsPage1To9.stream().forEach(System.out::println);
+        System.out.println("-------------------------");
+
+        services.pagenation(students,7,8,"F").forEach(System.out::println);
+        System.out.println("-------------------------");
+        List<Student> list1=services.pagenation(students,1,5,"F");
+        services.orderByMarks(list1).forEach(System.out::println);
+        System.out.println("-------------------------");
+        List<Student> list2 = services.pagenation(students, 9, 50, "F");
+        services.orderByMarks(list2).forEach(System.out::println);
 
     }
     static List<Student> uploadStudent(String filePath){
--- src/Assignment1_6/Services.java
+++ src/Assignment1_6/Services.java
@@ -1,8 +1,10 @@
 package Assignment1_6;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Services {
@@ -19,32 +21,115 @@ public class Services {
         return     studentList.stream().filter(student -> student.getMarks()>=50).collect(Collectors.toList());
     }
     void reportCard(List<Student> studentList){
-        List<Student> failedstudents = listOfStudentfaile(studentList);
-        List<Student> sortStudent = studentList.stream().sorted(Comparator.comparing(Student::getMarks)).collect(Collectors.toList());
-        for(Student s:sortStudent){
-            System.out.println(sortStudent.getClass()+"-----"+s.getName()+"-------");
+        List<Student> studentList1 = listOfStudentPassed(studentList);
+        Map<Student,String> report=new HashMap<>();
+        List<Student> sortedList = studentList1.stream().sorted(Comparator.comparing(Student::getMarks).reversed()).collect(Collectors.toList());
 
+        report.put(sortedList.get(0),"First Rank");
+        report.put(sortedList.get(1),"Second Rank");
+        report.put(sortedList.get(2),"Third Rank");
+
+        for (Student student : (sortedList.stream().skip(3).filter(e -> e.getMarks() >= 50).collect(Collectors.toList()))) {
+            report.put(student,"pass");
         }
-
-        List<Student> passedStudent = listOfStudentPassed(studentList);
-        Student max = passedStudent.stream().max(Comparator.comparing(Student::getMarks, (s1, s2) -> s2.compareTo(s1))).get();
-        return;
+        for (Student student : (studentList.stream().filter(e -> e.getMarks() < 50).collect(Collectors.toList()))) {
+            report.put(student,"fail");
+        }
+        report.forEach((k,v)-> System.out.println(k+" ::Status:"+v));
     }
     List<Student> listOfStudentfaile(List<Student> studentList){
         return     studentList.stream().filter(student -> student.getMarks()<50).collect(Collectors.toList());
     }
     List<Student> findStudentByClass(List<Student> studentList,List<Class> classList,String str){
         List<Integer> sectionId = classList.stream().filter(e ->str.equals(e.getName())).map(e -> e.getId()).collect(Collectors.toList());
-        System.out.println(sectionId);
         return studentList.stream().filter(student -> sectionId.contains(student.getClassId())).collect(Collectors.toList());
 
     }
     List<Student> failByAge(List<Student> studentList){
         return studentList.stream().filter(student -> student.getAge()>20).collect(Collectors.toList());
     }
-    void deleteStudent(int studentId , List<Student>studentList,List<Address> addressList){
-        List<Student> list  = studentList.stream().dropWhile(e -> e.getId() == studentId).collect(Collectors.toList());
-        List<Address>addressLIst = addressList.stream().dropWhile(address -> address.getStudentId()==studentId).collect(Collectors.toList());
+    void deleteStudentFEromCsv(String filePathStudent ,String filePathAddress,int studentId,List<Address> addressList){
+        // Read the CSV file into memory and exclude the line to remove
 
+        List<String> lines = new ArrayList<>();
+        try  {
+            BufferedReader reader = new BufferedReader(new FileReader(filePathStudent));
+            String line;
+            int lineNumber = 0;
+            if((line=reader.readLine())!=null) {
+                lines.add(line);
+            }
+            while ((line = reader.readLine()) != null) {
+
+                String [] student=line.split(",");
+
+                if (Integer.parseInt(student[0]) !=studentId ) {
+                    lines.add(line);
+                }
+                lineNumber++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Write the modified data back to the CSV file
+        try  {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePathStudent));
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine(); // Add a newline character to separate lines
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Student removed successfully.");
+        try  {
+            BufferedReader reader = new BufferedReader(new FileReader(filePathAddress));
+            String line;
+            int lineNumber = 0;
+            if((line=reader.readLine())!=null) {
+                lines.add(line);
+            }
+            while ((line = reader.readLine()) != null) {
+
+                String [] student=line.split(",");
+
+                if (Integer.parseInt(student[3]) !=studentId ) {
+                    lines.add(line);
+                }
+                lineNumber++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Write the modified data back to the CSV file
+        try  {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePathAddress));
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine(); // Add a newline character to separate lines
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    List<Student> pagenation(List<Student> students, long startPoint,long endPoint,String gender ){
+      return students.stream().skip(startPoint-1).limit(endPoint-startPoint+1).filter(student -> student.getGender().equals(gender))
+                .collect(Collectors.toList());
+    }
+    List<Student> orderByMarks(List<Student> studentList){
+        return studentList.stream().sorted(Comparator.comparing(Student::getMarks).reversed()).collect(Collectors.toList());
+    }
+    List<Student> filterByClass(List<Student> studentList,List<Class> classList,String section){
+        int  classId = classList.stream().filter(cls -> cls.getName().equals(section)).map(c->c.getId()).findFirst().get();
+
+        return studentList.stream().filter(student->student.getClassId()==classId).collect(Collectors.toList());
+    }
+    List<Student> filterByGender(List<Student> studentList,List<Class> classList,String gender){
+
+        return studentList.stream().filter(student->student.getGender().equals(gender)).collect(Collectors.toList());
     }
 }
